551014436c586ad0d46fc76ffb16dffcdac6c7c9
radiansz <lightwaveinthespace@gmail.com>
2017-06-22 23:50:18 +0300

    feat(motion): add motion model

---
+++ Assets/Scripts/Models/MotionModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionModel : MonoBehaviour {
+
+    public enum Rotation { stale, left, right };
+    public enum Acceleration { stale, forward, backward, left, right };
+
+    Rotation currentRotation = Rotation.stale;
+    Acceleration currentAcceleration = Acceleration.stale;
+
+    public Rotation rotation
+    {
+        set
+        {
+            Debug.Log(value);
+            currentRotation = value;
+        }
+        get
+        {
+            return currentRotation;
+        }
+    }
+
+    public Acceleration acceleration
+    {
+        set
+        {
+            Debug.Log(value);
+            currentAcceleration = value;
+        }
+        get
+        {
+            return currentAcceleration;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
